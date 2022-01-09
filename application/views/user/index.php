056a456d22b4b06905e0a3a2ceddf0f9fef29d9b
Ubaidillah Aminuddin Thoyieb <ateritori@gmail.com>
2022-01-09 21:15:36 +0700

    Update index.php

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -117,7 +117,7 @@
                         </div>
                         <div class="modal-footer">
                             <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                            <a class="btn btn-primary" href="#">Simpan</a>
+                            <a class="btn btn-primary" href="<?= base_url('user/crud'); ?>">Simpan</a>
                         </div>
                 </form>
             </div>
